64f40a631afc26a4311b8cfabb8d88e8c8f401ec
RafiiArifNugroho <rafialamcitra@gmail.com>
2026-04-28 15:38:25 +0700

    Menambahkan implementasi method displayAll, setNama, dan setId

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -13,12 +13,35 @@ public:
     friend class soiswa;
 };
 
-class siswa 
+class siswa
 {
 private:
     int id;
 
-public:    
+public:
     void setId(int pId);
     void showplayAll(orang &a);
 };
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl
+         << a.nama << endl;
+}
+void orang ::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
+}
